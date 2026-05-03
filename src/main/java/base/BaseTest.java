dcd74b018eea95f33846b3643dbee655cdb38905
Rajkumar <rkr15250328@gmail.com>
2026-05-03 16:36:08 +0530

    added log4j2

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,20 +5,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
 public class BaseTest {
 	
 	protected WebDriver driver;
 	
 	@BeforeMethod
 	public void setup() {
+		Log.info("Starting webdriver");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
+		Log.info("Navigate to URL...");
 		driver.get("https://admin-demo.nopcommerce.com/login");		
 	}
 	
 	@AfterMethod	
 	public void tearDown() {
 		if(driver!=null) {
+			Log.info("Closing the Browser");
 			driver.quit();
 		}
 	}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,16 +5,21 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest{
 	
 	@Test	
 	public void testValidLogin() {
 		
+		Log.info("Starting Loging Test");
+		
 		LoginPage loginPage = new LoginPage(driver);
+		Log.info("Adding Credentials");
 		loginPage.enterUserName("admin@yourstore.com");
 		loginPage.enterPassword("admin");
 		loginPage.clickLogin();
+		Log.info("Verify the login Scenario");
 		Assert.assertEquals(driver.getTitle(), "Just a moment...");		
 		
 		
