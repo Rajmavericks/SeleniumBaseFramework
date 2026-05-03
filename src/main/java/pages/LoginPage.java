10dac573a32f90571b40ca4a3861b3f7976481a5
Rajkumar <rkr15250328@gmail.com>
2026-05-03 18:55:22 +0530

    page factory modifciation

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,33 +2,46 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	
 	private WebDriver driver;
 	
-	private By userIdfield= By.cssSelector("input[data-qa='login-email']");
-	private By passwordfield = By.cssSelector("input[placeholder='Password']");
-	private By LoginBtn=By.cssSelector("button[data-qa='login-button']");
+	@FindBy(css = "input[data-qa='login-email']" )
+	WebElement userIdfield;
 	
+	@FindBy(css= "input[placeholder='Password']")
+	WebElement passwordfield;
+	
+	@FindBy(css = "button[data-qa='login-button']")
+	WebElement LoginBtn;
+	
+	
+	
+
 	public LoginPage(WebDriver driver) {
 		this.driver=driver;
+		PageFactory.initElements(driver, this);
 		
 	}
 	
 	public void enterUserName(String username) {
-		driver.findElement(userIdfield).clear();
-		driver.findElement(userIdfield).sendKeys(username);
+		
+		userIdfield.clear();		
+		userIdfield.sendKeys(username);
 		
 	}
 	
 	public void enterPassword(String password) {
-		driver.findElement(passwordfield).clear();
-		driver.findElement(passwordfield).sendKeys(password);		
+		passwordfield.clear();
+		passwordfield.sendKeys(password);		
 	}
 	
 	public void clickLogin() {
-		driver.findElement(LoginBtn).click();
+		LoginBtn.click();
 		
 	}
 
